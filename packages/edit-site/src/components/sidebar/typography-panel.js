fcb6391ab11a999923fad4abebc22f4d078f0881
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-07-28 17:34:30 +1000

    Switch typography block support to use ToolsPanel

--- packages/edit-site/src/components/sidebar/typography-panel.js
+++ packages/edit-site/src/components/sidebar/typography-panel.js
@@ -6,8 +6,14 @@ import {
 	__experimentalFontFamilyControl as FontFamilyControl,
 	__experimentalFontAppearanceControl as FontAppearanceControl,
 	__experimentalLetterSpacingControl as LetterSpacingControl,
+	__experimentalTextDecorationControl as TextDecorationControl,
+	__experimentalTextTransformControl as TextTransformControl,
 } from '@wordpress/block-editor';
-import { PanelBody, FontSizePicker } from '@wordpress/components';
+import {
+	__experimentalToolsPanel as ToolsPanel,
+	__experimentalToolsPanelItem as ToolsPanelItem,
+	FontSizePicker,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -19,11 +25,17 @@ export function useHasTypographyPanel( { supports, name } ) {
 	const hasLineHeight = useHasLineHeightControl( { supports, name } );
 	const hasFontAppearance = useHasAppearanceControl( { supports, name } );
 	const hasLetterSpacing = useHasLetterSpacingControl( { supports, name } );
+	const hasTextDecoration = useHasTextDecorationControl( { supports, name } );
+	const hasTextTransform = useHasTextTransformControl( { supports, name } );
+
 	return (
 		hasLineHeight ||
 		hasFontAppearance ||
 		hasLetterSpacing ||
-		supports.includes( 'fontSize' )
+		hasTextDecoration ||
+		hasTextTransform ||
+		supports.includes( 'fontSize' ) ||
+		supports.includes( 'fontFamily' )
 	);
 }
 
@@ -33,7 +45,6 @@ function useHasLineHeightControl( { supports, name } ) {
 		supports.includes( 'lineHeight' )
 	);
 }
-
 function useHasAppearanceControl( { supports, name } ) {
 	const hasFontStyles =
 		useSetting( 'typography.customFontStyle', name ) &&
@@ -42,6 +53,20 @@ function useHasAppearanceControl( { supports, name } ) {
 		useSetting( 'typography.customFontWeight', name ) &&
 		supports.includes( 'fontWeight' );
 	return hasFontStyles || hasFontWeights;
+}
+
+function useHasTextDecorationControl( { supports, name } ) {
+	return (
+		useSetting( 'typography.customTextDecoration', name ) &&
+		supports.includes( 'textDecoration' )
+	);
+}
+
+function useHasTextTransformControl( { supports, name } ) {
+	return (
+		useSetting( 'typography.customTextTransform', name ) &&
+		supports.includes( 'textTransform' )
+	);
 }
 
 function useHasLetterSpacingControl( { supports, name } ) {
@@ -56,76 +81,193 @@ export default function TypographyPanel( {
 	getStyle,
 	setStyle,
 } ) {
+	// To better reflect if the user has customized a value we need to
+	// ensure the style value being checked is from the `user` origin.
+	const createHasValueCallback = ( feature ) => () =>
+		!! getStyle( name, feature, 'user' );
+
+	const createResetCallback = ( feature ) => () =>
+		setStyle( name, feature, undefined );
+
+	const handleOnChange = ( feature ) => ( value ) => {
+		setStyle( name, feature, value );
+	};
+
+	// Font Family.
+	const fontFamilies = useSetting( 'typography.fontFamilies', name );
+	const showFontFamilyControl = supports.includes( 'fontFamily' );
+
+	// Font Size.
 	const fontSizes = useSetting( 'typography.fontSizes', name );
+	const showFontSizeControl = supports.includes( 'fontSize' );
 	const disableCustomFontSizes = ! useSetting(
 		'typography.customFontSize',
 		name
 	);
-	const fontFamilies = useSetting( 'typography.fontFamilies', name );
+
+	// Font Appearance.
+	const showAppearanceControl = useHasAppearanceControl( { supports, name } );
+	const fontStyle = getStyle( name, 'fontStyle' );
+	const fontWeight = getStyle( name, 'fontWeight' );
+	const hasFontAppearanceValue = () => !! fontStyle || !! fontWeight;
+	const resetFontAppearance = () => {
+		setStyle( name, 'fontStyle', undefined );
+		setStyle( name, 'fontWeight', undefined );
+	};
+
 	const hasFontStyles =
 		useSetting( 'typography.customFontStyle', name ) &&
 		supports.includes( 'fontStyle' );
+
 	const hasFontWeights =
 		useSetting( 'typography.customFontWeight', name ) &&
 		supports.includes( 'fontWeight' );
-	const hasLineHeightEnabled = useHasLineHeightControl( { supports, name } );
-	const hasAppearanceControl = useHasAppearanceControl( { supports, name } );
-	const hasLetterSpacingControl = useHasLetterSpacingControl( {
+
+	// Line Height.
+	const showLineHeightControl = useHasLineHeightControl( { supports, name } );
+
+	// Text Decoration.
+	const showTextDecoration = useHasTextDecorationControl( {
 		supports,
 		name,
 	} );
 
+	// Text Transform.
+	const showTextTransform = useHasTextTransformControl( { supports, name } );
+
+	// Letter Spacing.
+	const showLetterSpacingControl = useHasLetterSpacingControl( {
+		supports,
+		name,
+	} );
+
+	// Reset all the typography related global styles.
+	const resetAll = () => {
+		setStyle( name, 'fontFamily', undefined );
+		setStyle( name, 'fontSize', undefined );
+		setStyle( name, 'fontStyle', undefined );
+		setStyle( name, 'fontWeight', undefined );
+		setStyle( name, 'lineHeight', undefined );
+		setStyle( name, 'textDecoration', undefined );
+		setStyle( name, 'textTransform', undefined );
+		setStyle( name, 'letterSpacing', undefined );
+	};
+
 	return (
-		<PanelBody title={ __( 'Typography' ) } initialOpen={ true }>
-			{ supports.includes( 'fontFamily' ) && (
-				<FontFamilyControl
-					fontFamilies={ fontFamilies }
-					value={ getStyle( name, 'fontFamily' ) }
-					onChange={ ( value ) =>
-						setStyle( name, 'fontFamily', value )
-					}
-				/>
+		<ToolsPanel
+			label={ __( 'Typography options' ) }
+			header={ __( 'Typography' ) }
+			resetAll={ resetAll }
+			className="typography-controls"
+		>
+			{ showFontFamilyControl && (
+				<ToolsPanelItem
+					hasValue={ createHasValueCallback( 'fontFamily' ) }
+					label={ __( 'Font family' ) }
+					onDeselect={ createResetCallback( 'fontFamily' ) }
+					isShownByDefault={ true }
+				>
+					<FontFamilyControl
+						fontFamilies={ fontFamilies }
+						value={ getStyle( name, 'fontFamily' ) }
+						onChange={ handleOnChange( 'fontFamily' ) }
+					/>
+				</ToolsPanelItem>
 			) }
-			{ supports.includes( 'fontSize' ) && (
-				<FontSizePicker
-					value={ getStyle( name, 'fontSize' ) }
-					onChange={ ( value ) =>
-						setStyle( name, 'fontSize', value )
-					}
-					fontSizes={ fontSizes }
-					disableCustomFontSizes={ disableCustomFontSizes }
-				/>
+			{ showFontSizeControl && (
+				<ToolsPanelItem
+					hasValue={ createHasValueCallback( 'fontSize' ) }
+					label={ __( 'Font size' ) }
+					onDeselect={ createResetCallback( 'fontSize' ) }
+					isShownByDefault={ true }
+				>
+					<FontSizePicker
+						value={ getStyle( name, 'fontSize' ) }
+						onChange={ handleOnChange( 'fontSize' ) }
+						fontSizes={ fontSizes }
+						disableCustomFontSizes={ disableCustomFontSizes }
+						allowReset={ false }
+					/>
+				</ToolsPanelItem>
 			) }
-			{ hasLineHeightEnabled && (
-				<LineHeightControl
-					value={ getStyle( name, 'lineHeight' ) }
-					onChange={ ( value ) =>
-						setStyle( name, 'lineHeight', value )
-					}
-				/>
+			{ showAppearanceControl && (
+				<ToolsPanelItem
+					className="single-column"
+					hasValue={ hasFontAppearanceValue }
+					label={ __( 'Appearance' ) }
+					onDeselect={ resetFontAppearance }
+					isShownByDefault={ true }
+				>
+					<FontAppearanceControl
+						value={ { fontStyle, fontWeight } }
+						onChange={ ( {
+							fontStyle: newFontStyle,
+							fontWeight: newFontWeight,
+						} ) => {
+							setStyle( name, 'fontStyle', newFontStyle );
+							setStyle( name, 'fontWeight', newFontWeight );
+						} }
+						hasFontStyles={ hasFontStyles }
+						hasFontWeights={ hasFontWeights }
+					/>
+				</ToolsPanelItem>
 			) }
-			{ hasAppearanceControl && (
-				<FontAppearanceControl
-					value={ {
-						fontStyle: getStyle( name, 'fontStyle' ),
-						fontWeight: getStyle( name, 'fontWeight' ),
-					} }
-					onChange={ ( { fontStyle, fontWeight } ) => {
-						setStyle( name, 'fontStyle', fontStyle );
-						setStyle( name, 'fontWeight', fontWeight );
-					} }
-					hasFontStyles={ hasFontStyles }
-					hasFontWeights={ hasFontWeights }
-				/>
+			{ showLineHeightControl && (
+				<ToolsPanelItem
+					className="single-column"
+					hasValue={ createHasValueCallback( 'lineHeight' ) }
+					label={ __( 'Line height' ) }
+					onDeselect={ createResetCallback( 'lineHeight' ) }
+					isShownByDefault={ true }
+				>
+					<LineHeightControl
+						value={ getStyle( name, 'lineHeight' ) }
+						onChange={ handleOnChange( 'lineHeight' ) }
+					/>
+				</ToolsPanelItem>
 			) }
-			{ hasLetterSpacingControl && (
-				<LetterSpacingControl
-					value={ getStyle( name, 'letterSpacing' ) }
-					onChange={ ( value ) =>
-						setStyle( name, 'letterSpacing', value )
-					}
-				/>
+			{ showTextDecoration && (
+				<ToolsPanelItem
+					className="single-column"
+					hasValue={ createHasValueCallback( 'textDecoration' ) }
+					label={ __( 'Decoration' ) }
+					onDeselect={ createResetCallback( 'textDecoration' ) }
+					isShownByDefault={ true }
+				>
+					<TextDecorationControl
+						value={ getStyle( name, 'textDecoration' ) }
+						onChange={ handleOnChange( 'textDecoration' ) }
+					/>
+				</ToolsPanelItem>
 			) }
-		</PanelBody>
+			{ showTextTransform && (
+				<ToolsPanelItem
+					className="single-column"
+					hasValue={ createHasValueCallback( 'textTransform' ) }
+					label={ __( 'Letter case' ) }
+					onDeselect={ createResetCallback( 'textTransform' ) }
+					isShownByDefault={ true }
+				>
+					<TextTransformControl
+						value={ getStyle( name, 'textTransform' ) }
+						onChange={ handleOnChange( 'textTransform' ) }
+					/>
+				</ToolsPanelItem>
+			) }
+			{ showLetterSpacingControl && (
+				<ToolsPanelItem
+					className="single-column"
+					hasValue={ createHasValueCallback( 'letterSpacing' ) }
+					label={ __( 'Letter-spacing' ) }
+					onDeselect={ createResetCallback( 'letterSpacing' ) }
+					isShownByDefault={ true }
+				>
+					<LetterSpacingControl
+						value={ getStyle( name, 'letterSpacing' ) }
+						onChange={ handleOnChange( 'letterSpacing' ) }
+					/>
+				</ToolsPanelItem>
+			) }
+		</ToolsPanel>
 	);
 }
