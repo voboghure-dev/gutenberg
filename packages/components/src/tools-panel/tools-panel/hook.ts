aabb10a542b8ce3ddbdddcdebc7290422462ef7d
ramonjd <ramonjd@gmail.com>
2021-09-16 08:14:18 +1000

    updating variable name to make it clearer that we want to track if optional items are available at all *and* if so, are they hidden

--- packages/components/src/tools-panel/tools-panel/hook.ts
+++ packages/components/src/tools-panel/tools-panel/hook.ts
@@ -95,8 +95,8 @@ export function useToolsPanel(
 
 	// Track whether optional controls, if any, are displayed or not.
 	const [
-		areOptionalControlsHidden,
-		setAreOptionalControlsHidden,
+		areOptionalControlsAvailableAndHidden,
+		setAreOptionalControlsAvailableAndHidden,
 	] = useState( false );
 
 	// Where no optional menu items are active, we display a plus icon
@@ -106,10 +106,10 @@ export function useToolsPanel(
 			const optionalMenuItemsArray = Object.entries( menuItems.optional );
 			const newValue =
 				optionalMenuItemsArray.length > 0 &&
-				! Object.entries( menuItems.optional ).some(
+				! optionalMenuItemsArray.some(
 					( [ , isSelected ] ) => isSelected
 				);
-			setAreOptionalControlsHidden( newValue );
+			setAreOptionalControlsAvailableAndHidden( newValue );
 		}
 	}, [ menuItems.optional ] );
 
@@ -184,7 +184,7 @@ export function useToolsPanel(
 		registerPanelItem,
 		deregisterPanelItem,
 		flagItemCustomization,
-		areOptionalControlsHidden,
+		areOptionalControlsAvailableAndHidden,
 		hasMenuItems: !! panelItems.length,
 		isResetting: isResetting.current,
 	};
